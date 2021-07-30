158e5081a963ebbeb639d7363e0a7c66cfaf5f54
Nguyễn Quốc Khánh <80594865+BruceSteven@users.noreply.github.com>
2021-07-30 23:23:11 +0700

    Update Sol.py

--- Codeforces Round #735 (Div. 2)/Prob. A Cherry/Sol.py
+++ Codeforces Round #735 (Div. 2)/Prob. A Cherry/Sol.py
@@ -1,1 +1,10 @@
-
+import sys 
+input = sys.stdin.buffer.readline
+t = int(input())
+for i in range(t):
+    n = int(input())
+    a = list(map(int,input().strip().split()))
+res = 0
+for i in range(n-1):
+    res = max(res,a[i]*a[i+1]);
+print(res)
