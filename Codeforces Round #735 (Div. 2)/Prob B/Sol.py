060524fe575f9c533b16076468bddc77706090eb
Nguyễn Quốc Khánh <80594865+BruceSteven@users.noreply.github.com>
2021-08-01 20:48:05 +0700

    Update Sol.py

--- Codeforces Round #735 (Div. 2)/Prob B/Sol.py
+++ Codeforces Round #735 (Div. 2)/Prob B/Sol.py
@@ -1,1 +1,12 @@
-
+import sys
+input = sys.stdin.buffer.readline
+t = int(input())
+for _ in range(t):
+    n,k = map(int,input().split())
+    a = list(map(int,input().strip().split()))
+    l = max(0,n-2*k-1)
+    ans = -1e12 
+    for i in range(l,n):
+        for j in range(i+1,n):
+                ans = max(ans,(i+1)*(j+1)-k*(a[i]|a[j]))
+    print(ans)
